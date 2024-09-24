62ef98461acb208d49a74fbf576e2321f92809bf
ViktoriiaYakaba <yakaba@bk.ru>
2024-09-24 17:21:00 +0200

    add component

---
+++ src/components/teachersCard/TeachersCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TeachersCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TeachersCard;
---
+++ src/components/teachersFilter/TeaachersFilter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TeaachersFilter = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TeaachersFilter;
